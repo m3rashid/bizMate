3e8a6c30b1baa0b3c87b8c9d2b84d61769fe0e40
m3rashid <m3rashid.hussain@gmail.com>
2024-09-02 22:13:19 +0530

    updates

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,15 +1,5 @@
-import remarkGfm from 'remark-gfm'
-import createMDX from '@next/mdx'
-import rehypePrettyCode from 'rehype-pretty-code'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withMdx = createMDX({
-	options: {
-		remarkPlugins: [remarkGfm],
-		rehypePlugins: [rehypePrettyCode],
-	}
-})
 
-export default withMdx(nextConfig);
+export default nextConfig;
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
 };
 
 export default config;
